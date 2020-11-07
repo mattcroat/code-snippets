3f1815ebb5ab997158435154f1b1547261c0a1fc
mattcroat <mattcroat.dev@gmail.com>
2020-11-07 21:52:05 +0100

    Add creating of snippets to API route

--- pages/api/createSnippet.js
+++ pages/api/createSnippet.js
@@ -6,8 +6,15 @@ const handler = async (req, res) => {
   if (req.method !== 'POST') {
     return res.status(405).json({ msg: 'Method not allowed' })
   }
+
   try {
-    //TODO: create the snippet
+    const createdSnippet = await createSnippet(
+      code,
+      language,
+      description,
+      name
+    )
+    return res.status(200).json(createdSnippet)
   } catch (err) {
     console.error(err)
     res.status(500).json({ msg: 'Something went wrong.' })
